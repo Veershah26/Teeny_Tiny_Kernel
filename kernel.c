a13a41be91c0360f598dbb289f248e8162b2437e
Veershah26 <shah.veer18@gmail.com>
2021-08-01 17:10:24 +0530

    Created necessary variables for kernel.c, and the screen clear loop, signing off for now

---
+++ kernel.c
@@ -0,0 +1,13 @@
+void kmain(void) 
+{
+    const char *str = "Teeny Tiny Kernel, Now I Dont Know If This C File Will Run In Real Mode Or Protected Mode";
+    char *vidptr = (char*)0xb8000;
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    //while (j < 80 * 25 * 2) {
+      //  vidptr[j] = ' ';
+        //vidptr[j + 1] = 0x07;
+        //j = j + 2;
+   //}
+}
